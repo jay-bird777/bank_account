956e94f31ac8a448fa8dcc75bfc20ecb8cc6388f
jay-bird777 <67336371+jay-bird777@users.noreply.github.com>
2020-12-09 18:35:38 -0800

    second commit i think

--- baba's money.py
+++ baba's money.py
@@ -1,19 +1,21 @@
 
 
 
-p = float(input("what is the principle? "))
-r = float(input("what is the rate? "))
-n = int(input("how many periods "))
-t = int(input("how many payments per period "))
-pv = p * (pow((1 + r/100/n),n*t))
 
 
-print(round(pv))
 
-def compound_interest(p,r,n,t):
+
+
+def compound_interest():
+    p = float(input("what is the principle? "))
+    r = float(input("what is the rate? "))
+    n = int(input("how many payments per year? "))
+    t = int(input("how many years? "))
+    pv = p * (pow((1 + r/100/n),n*t))
     balance = p * (pow((1 + r/100/n),n*t))
     ci = balance - p
     print('Balance at the end of the period',round(balance,2))
     print("Intrest earned is", round(ci,2))
-
-compound_intrest(5000,10,12,5)
+    roi = (ci/balance) * 100
+    print("This is", round(roi,2),"% return on investment.")
+compound_interest()
